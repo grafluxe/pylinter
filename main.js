29a7f5bfd25ce6a6a8237de1c1d566e78433f0f2
Leandro Silva <lsilva@tremorvideo.com>
2016-07-19 16:37:40 -0400

    - Add info button (opens dialog with the full lint message). - Remove events on file change. - Clean up code.

--- main.js
+++ main.js
@@ -7,21 +7,24 @@
 
 define((require, exports, module) => {
     "use strict";
-    
+
     var AppInit = brackets.getModule("utils/AppInit"),
-        MainViewManager = brackets.getModule("view/MainViewManager"),
-        DocumentManager = brackets.getModule("document/DocumentManager"),
+        DocumentManager,
         EditorManager,
-        
-        init,
+        Dialogs,
+
+        fileChange,
+        setup,
         definePrefs,
         createPanel,
         parseOutput,
         setClickListeners,
         onLintComplete,
         onLintFail,
+        onLintMsg,
         execNode,
-        
+        clean,
+
         ran,
         nodeDomain,
         panel,
@@ -33,100 +36,112 @@ define((require, exports, module) => {
         lastFileWasPy,
         pythonPath,
         outputPattern;
-    
-    const DEFAULT_PYLINT_PATH = "/usr/local/bin/pylint",
+
+    const DEFAULT_PYLINT_PATH = "/usr/spltal/bin/pylint",
           DEFAULT_OUTPUT_PATTERN = "{msg_id} > {msg} [{symbol} @ {line},{column}]";
-    
-    AppInit.appReady(() => {        
-        MainViewManager.on("currentFileChange", () => {
-            if (panel) {
-                panel.hide();
-            }
-                        
-            //If not a Python file, exit
-            if (DocumentManager.getCurrentDocument().getLanguage().getName() !== "Python") {
-                lastFileWasPy = false;
-                DocumentManager.off("documentSaved", execNode);
-                
-                return;
-            }
-            
+
+    AppInit.appReady(() => {
+        var MainViewManager = brackets.getModule("view/MainViewManager");
+
+        DocumentManager = brackets.getModule("document/DocumentManager");
+
+        MainViewManager.on("currentFileChange", fileChange);
+    });
+
+    fileChange = () => {
+        var currDoc = DocumentManager.getCurrentDocument();
+
+        if (panel) {
+            panel.hide();
+        }
+
+        if (currDoc && currDoc.getLanguage().getName() === "Python") {
             if (!ran) {
                 ran = true;
-                
-                init();
+
+                setup();
                 definePrefs();
                 createPanel();
-                nodeDomain.on("lintComplete", onLintComplete);
-                nodeDomain.on("lintFail", onLintFail);
             }
-            
+
             editor = EditorManager.getActiveEditor();
-            
-            execNode();
-            
+
+            $panelBody.find("a").off("click");
+
             if (!lastFileWasPy) {
                 lastFileWasPy = true;
 
+                nodeDomain.on("lintComplete", onLintComplete);
+                nodeDomain.on("lintFail", onLintFail);
+                nodeDomain.on("lintMsg", onLintMsg);
                 DocumentManager.on("documentSaved", execNode);
             }
-        });
-    });
-    
-    init = () => {
+
+            execNode();
+        } else {
+            clean();
+        }
+    };
+
+    setup = () => {
         var NodeDomain = brackets.getModule("utils/NodeDomain"),
             ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
-        
+
         ExtensionUtils.loadStyleSheet(module, "view/style.css");
-        
+
         EditorManager = brackets.getModule("editor/EditorManager");
+        Dialogs = brackets.getModule("widgets/Dialogs");
         nodeDomain = new NodeDomain("nodeLogic", ExtensionUtils.getModulePath(module, "node-context"));
     };
-    
+
     definePrefs = () => {
         var PreferencesManager = brackets.getModule("preferences/PreferencesManager"),
             prefs = PreferencesManager.getExtensionPrefs("pylinter");
-        
+
         pythonPath = prefs.get("pythonPath");
         outputPattern = prefs.get("outputPattern");
-        
+
         if (!pythonPath) {
             pythonPath = DEFAULT_PYLINT_PATH;
             prefs.set("pythonPath", pythonPath);
         }
-        
+
         if (!outputPattern) {
             outputPattern = DEFAULT_OUTPUT_PATTERN;
             prefs.set("outputPattern", outputPattern);
         }
     };
-    
+
     createPanel = () => {
         var WorkspaceManager = brackets.getModule("view/WorkspaceManager");
-        
+
         panel = WorkspaceManager.createBottomPanel("pylint-panel", $(require("text!view/panel.html")), 144);
-        
+
         $panelFilename = $("#pylint-filename");
         $panelBody = $("#pylint-body");
         $panelCloseBtn = $("#pylint-close-btn");
-            
+
         $panelCloseBtn.click(() => panel.hide());
     };
-    
-    onLintComplete = (e, data) => {
+
+    onLintComplete = (e, msg) => {
         var html,
             issueCount;
 
-        if (data) {
-            parsed = parseOutput(data);
+        console.log("Xxx");
+
+        if (msg) {
+            console.log(msg.substr(0, 200), "...");
+
+            parsed = parseOutput(msg);
             html = "<ul>";
 
             parsed.out.forEach(function (el, i) {
-                html += `<li><a href="#" data-pylint-el="${i}">${el.txt}</a></li>`;
+                html += `<li><a href="#" data-pylint-el="${i}" class="pylint-err">${el.txt}</a><a href="#" data-pylint-id="${el.msgId}" class="pylint-more"><span>?</span></a></li>`;
             });
 
             html += "</ul>";
-            
+
             issueCount = parsed.out.length;
 
             $panelFilename.text(parsed.name + ` (${issueCount} issue${issueCount > 1 ? "s" : ""})`);
@@ -137,40 +152,49 @@ define((require, exports, module) => {
             panel.show();
         } else {
             $panelFilename.text("");
-            $panelBody.html("No Issues");
+            $panelBody.find("a").off("click").html("No Issues");
             panel.hide();
         }
     };
-    
-    onLintFail = (e, data) => {
-        var Dialogs = brackets.getModule("widgets/Dialogs");
-        
+
+    onLintFail = (e, err) => {
         Dialogs.showModalDialog(
             null,
             "Pylinter",
             "<b>Pylint can't be found!</b><br>Please confirm you have Pylint installed and that the property 'pylinter.pythonPath' in your Brackets preference file is correct. Once you edit your preference file, please restart Brackets."
         );
-        
-        console.error("[Pylinter] Node Error: ", data);
+
+        console.error("[Pylinter] Node Error: ", err);
     };
-    
+
+    onLintMsg = (e, msg) => {
+        var [, title, info, desc] = msg.match(/:(.+): \*(.+)\*([\s\S]+)/);
+
+        Dialogs.showModalDialog(
+            null,
+            "Pylinter",
+            `<h4>${title}</h4><p>${info}</p><p>${desc}</p>`
+        );
+    };
+
     parseOutput = (input) => {
         var name = input.match(/^\** .* (.*)$/m)[1],
             out,
-            loc;
+            splt;
 
         out = input.split(/\n/);
-        
+
         out.shift();
         out.pop();
 
         out = out.map((el, i) => {
-            loc = el.match(/^(-?\d+),(-?\d+)\?/);
-            
+            splt = el.match(/^(-?\d+),(-?\d+);(.+)\?/);
+
             return {
-                txt: $("<p>" + el.substr(el.indexOf("?") + 1)).text(),
-                line: Number(loc[1]) - 1,
-                col: Math.max(Number(loc[2]), 0)
+                txt: $("<p>" + el.substr(el.indexOf("?") + 1) + "</p>").text(),
+                line: Number(splt[1]) - 1,
+                col: Math.max(Number(splt[2]), 0),
+                msgId: splt[3]
             };
         });
 
@@ -179,18 +203,38 @@ define((require, exports, module) => {
             out
         };
     };
-    
+
     setClickListeners = () => {
-        $panelBody.find("ul > li > a").click((e) => {
-            var indx = $(e.target).data("pylint-el"),
+        $panelBody.find(".pylint-err").click((e) => {
+            var indx = e.currentTarget.dataset.pylintEl,
                 el = parsed.out[indx];
-            
+
+            e.preventDefault();
+
             editor.setCursorPos(el.line, el.col, true);
             editor.focus();
         });
+
+        $panelBody.find(".pylint-more").click((e) => {
+            e.preventDefault();
+
+            nodeDomain.exec("getMsg", pythonPath, e.currentTarget.dataset.pylintId);
+        });
     };
-    
+
     execNode = () => {
         nodeDomain.exec("lintIt", pythonPath, DocumentManager.getCurrentDocument().file.fullPath, outputPattern);
+    };
+
+    clean = () => {
+        lastFileWasPy = false;
+
+        if (nodeDomain) {
+            nodeDomain.off("lintComplete", onLintComplete);
+            nodeDomain.off("lintFail", onLintFail);
+            nodeDomain.off("lintMsg", onLintMsg);
+        }
+
+        DocumentManager.off("documentSaved", execNode);
     };
 });
--- node-context.js
+++ node-context.js
@@ -4,40 +4,49 @@
 
 (function () {
     "use strict";
-    
-    var spawn = require("child_process").spawn,
-        domainMgr,
-        init,
-        lintIt;
-    
+
+    var init,
+        lintIt,
+        getMsg,
+
+        spawn = require("child_process").spawn,
+        domainMgr;
+
     init = function (domainManager) {
         domainMgr = domainManager;
-        
+
         if (!domainMgr.hasDomain("nodeLogic")) {
             domainMgr.registerDomain("nodeLogic", {
                 major: 0,
                 minor: 1
             });
         }
-        
+
         domainMgr.registerCommand(
             "nodeLogic", //domain name
             "lintIt", //command name
-            lintIt //command handler function            
+            lintIt //command handler function
         );
-        
+
+        domainMgr.registerCommand(
+            "nodeLogic",
+            "getMsg",
+            getMsg
+        );
+
         domainMgr.registerEvent("nodeLogic", "lintComplete");
         domainMgr.registerEvent("nodeLogic", "lintFail");
+        domainMgr.registerEvent("nodeLogic", "lintMsg");
     };
-    
-    lintIt = function (pylintPath, filePath, pattern) {        
-        var cmd = spawn(pylintPath, ["-rn", "--msg-template='{line},{column}?" + pattern + "'", filePath]),
+
+    lintIt = function (pylintPath, filePath, pattern) {
+        var cmd = spawn(pylintPath, ["-rn", "--msg-template='{line},{column};{msg_id}?" + pattern + "'", filePath]),
             out;
-        
+
         cmd.once("error", function (err) {
             domainMgr.emitEvent("nodeLogic", "lintFail", err);
         });
-        
+
         cmd.stdout.once("data", function (data) {
             out = data.toString();
         });
@@ -46,7 +55,19 @@
             domainMgr.emitEvent("nodeLogic", "lintComplete", out);
         });
     };
-    
+
+    getMsg = function (pylintPath, id) {
+        var cmd = spawn(pylintPath, ["--help-msg=" + id]);
+
+        cmd.once("error", function (err) {
+            domainMgr.emitEvent("nodeLogic", "lintFail", err);
+        });
+
+        cmd.stdout.once("data", function (data) {
+            domainMgr.emitEvent("nodeLogic", "lintMsg", data.toString());
+        });
+    };
+
     //
     exports.init = init;
 }());
